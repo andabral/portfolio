40e8b614403f2dc50ec716b4598c06fb236f9c74
Andabral <andabral@gmail.com>
2023-04-03 00:02:40 -0500

    Actualizar Datos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,11 +20,6 @@ function Home() {
                 </span>
               </h1>
 
-              <!--h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Angel Bracero</strong>
-              </h1-->
-
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
